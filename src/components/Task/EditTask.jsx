6b893c7b15ec416ec9fc48f98e1371468b3bd962
Businge931 <jimmybusinge9@gmail.com>
2023-07-03 17:16:35 +0300

    feat: edit task modal

---
+++ src/components/Task/EditTask.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import Modal from "../modal/Modal";
+import AddTask from "./AddTask";
+
+const EditTask = () => {
+  return <AddTask heading="Edit Task" actionType="Save Changes" />;
+};
+
+export default EditTask;

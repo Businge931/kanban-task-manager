e8ed87fce65681e54d50883305cea9869214e1ab
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 02:01:05 +0300

    feat: dark theme

---
+++ src/components/ui/Logo/Logo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Logo.module.css";
+
+import logoLight from "../../../assets/logo-dark.svg";
+import logoDark from "../../../assets/logo-light.svg";
+import { useThemeContext } from "../../../context/ThemeContext";
+
+const Logo = () => {
+  const { theme } = useThemeContext();
+  return (
+    <div
+      className={`${styles.logo} ${theme === "dark" ? styles.logo_dark : ""}`}
+    >
+      {theme === "light" ? (
+        <img alt="logo" src={logoLight} />
+      ) : (
+        <img alt="logo" src={logoDark} />
+      )}
+    </div>
+  );
+};
+
+export default Logo;

dd9522667b9a15025bbe49ce213a8d4d39efc428
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 01:50:28 +0300

    struct: changed file directory

--- src/components/Task/EditTask.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import Modal from "../modal/Modal";
-import AddTask from "./AddTask";
-
-const EditTask = () => {
-  return <AddTask heading="Edit Task" actionType="Save Changes" />;
-};
-
-export default EditTask;

b5bf94e0e18ce212d3f3f50f232ac8f02008ca76
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 02:01:56 +0300

    feat: added modal context

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -4,23 +4,16 @@ const contextProvider = createContext();
 
 const initialState = {
   event: "add_new_board",
-  tasks: [],
-  subtasks: [],
-  columns: [],
-  boards: [],
   openModal: false,
-  status: null,
-  isTaskComplete: false,
   edit_delete: false,
-  // title: "",
 };
 
 function reducer(state, action) {
+  //WHEN OPENING MODAL ONLY
   switch (action.type) {
     case "addNewBoard":
       return {
         ...state,
-        boards: action.payload,
         openModal: true,
         event: "add_new_board",
       };
@@ -33,42 +26,29 @@ function reducer(state, action) {
     case "addColumn":
       return {
         ...state,
-        columns: state.columns.push(action.payload),
         openModal: true,
       };
     case "addTask":
       return {
         ...state,
-        tasks: state.tasks.push(action.payload),
-        subtasks: state.subtasks.push(action.payload),
         openModal: true,
-        status: action.payload,
-        isTaskComplete: false,
         event: "add_task",
       };
     case "editTask":
       return {
         ...state,
-        tasks: [...state.tasks, action.payload],
-        subtasks: [...state.subtasks, action.payload],
         openModal: true,
-        status: action.payload,
-        isTaskComplete: action.payload,
         event: "edit_task",
       };
     case "viewTask":
       return {
         ...state,
-        status: action.payload,
-        isTaskComplete: action.payload,
         openModal: true,
         event: "view_task",
       };
     case "deleteTask":
-      const title = action.payload;
       return {
         ...state,
-        tasks: state.tasks.filter((task) => task.title !== title),
         openModal: true,
         event: "delete_task",
       };
@@ -87,29 +67,15 @@ function reducer(state, action) {
 export default function ModalContext({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const {
-    tasks,
-    columns,
-    subtasks,
-    openModal,
-    status,
-    isTaskComplete,
-    event,
-    edit_delete,
-  } = state;
+  const { openModal, event, edit_delete } = state;
 
   return (
     <contextProvider.Provider
       value={{
         dispatch,
-        tasks,
-        columns,
-        subtasks,
-        openModal,
-        status,
-        isTaskComplete,
         event,
         edit_delete,
+        openModal,
       }}
     >
       {children}
@@ -119,5 +85,7 @@ export default function ModalContext({ children }) {
 
 export function UseModalContext() {
   const context = useContext(contextProvider);
+  if (!context)
+    throw new Error("Component must be wrapped inside modalContext");
   return context;
 }

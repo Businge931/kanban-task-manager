817b02f417ae8605c04dd236de03538430658a78
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 02:04:25 +0300

    feat: fetched respective task data, add dark theme

---
+++ src/components/modalWindows/Tasks/ShowTaskDetails.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Modal from "../modal/Modal";
+import styles from "./ShowTaskDetails.module.css";
+import taskIcon from "../../../assets/header-dots-icon.svg";
+
+import { UseModalContext } from "../../../context/ModalContext";
+import { useThemeContext } from "../../../context/ThemeContext";
+import UseTasksContext from "../../../context/TasksContext";
+
+function ShowTaskDetails({ clickedTask }) {
+  const { dispatch, edit_delete } = UseModalContext();
+  const { theme } = useThemeContext();
+  const { boards: allBoards, currentBoard } = UseTasksContext();
+
+  const taskDetails =
+    allBoards &&
+    allBoards[currentBoard]?.columns
+      ?.flatMap((column) => column.tasks)
+      .find((task) => task.title === clickedTask);
+
+  const completedSubTasks = taskDetails?.subtasks?.filter(
+    (subtask) => subtask.isCompleted === true
+  ).length;
+
+  return (
+    <Modal>
+      <div className={styles.task}>
+        <div className={styles.heading}>
+          <h3 className={` ${theme === "dark" ? styles.dark : ""}`}>
+            {taskDetails?.title}
+          </h3>
+
+          <img
+            alt="icon"
+            src={taskIcon}
+            onClick={() => dispatch({ type: "manageTask" })}
+          />
+        </div>
+        {edit_delete && (
+          <div className={styles.manage_task}>
+            <p
+              onClick={() => {
+                dispatch({ type: "editTask" });
+              }}
+            >
+              Edit Task
+            </p>
+            <p
+              onClick={() => {
+                dispatch({ type: "deleteTask" });
+              }}
+              id={styles.delete_task}
+            >
+              Delete Task
+            </p>
+          </div>
+        )}
+        <p>{taskDetails?.description}</p>
+        <h5>
+          Subtasks ({completedSubTasks} of {taskDetails?.subtasks?.length})
+        </h5>
+
+        {taskDetails?.subtasks?.map((subtask) => (
+          <div
+            key={subtask.title}
+            className={`${styles.subtask} ${
+              theme === "dark" ? styles.dark : ""
+            } `}
+          >
+            <label>
+              <input type="checkbox" defaultChecked={subtask.isCompleted} />
+              <span
+                className={`${styles.label} ${
+                  subtask.isCompleted === false ? styles.isCompleted : ""
+                }`}
+              >
+                {subtask.title}
+              </span>
+            </label>
+          </div>
+        ))}
+
+        <h5>Current Status</h5>
+        <select>
+          <option>Doing</option>
+          <option>Todo</option>
+          <option>Done</option>
+        </select>
+      </div>
+    </Modal>
+  );
+}
+
+export default ShowTaskDetails;

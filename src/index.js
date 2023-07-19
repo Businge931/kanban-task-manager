e77bba03b17f041b920f76c14ef5940bde465643
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 01:52:50 +0300

    feat: add tasks provider context

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
 import ModalContext from "./context/ModalContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { TasksProvider } from "./context/TasksContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ModalContext>
-      {/* <BrowserRouter> */}
-      <App />
-      {/* </BrowserRouter> */}
-    </ModalContext>
+    <ThemeProvider>
+      <TasksProvider>
+        <ModalContext>
+          <App />
+        </ModalContext>
+      </TasksProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );

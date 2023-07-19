b333e53dacf53532ebd993e5d3310e22062a1e04
Businge931 <jimmybusinge9@gmail.com>
2023-07-20 02:01:38 +0300

    feat: theme context

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useThemeContext() {
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) {
+    throw new Error(
+      "Must be wrapped in Conponent must be wrapped inside theme context"
+    );
+  }
+
+  return themeContext;
+}

9da9dde3949b54e6d92b5f18b81a0906b9c6267d
Businge931 <jimmybusinge9@gmail.com>
2023-09-08 22:31:32 +0300

    feat: re-organise all component imports

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Sidebar } from "../components/sidebar/Sidebar";
+export { default as HeaderNav } from "../components/ui/HeaderNav/HeaderNav";
+export { default as Logo } from "../components/ui/Logo/Logo";
+export { default as ShowTaskDetails } from "../components/modalWindows/Tasks/ShowTaskDetails";
+export { default as AddTask } from "../components/modalWindows/Tasks/AddTask";
+export { default as EditTask } from "../components/modalWindows/Tasks/EditTask";
+export { default as DeleteTask } from "../components/modalWindows/Tasks/DeleteTask";
+export { default as AddNewBoard } from "../components/modalWindows/Board/AddNewBoard";
+export { default as EditBoard } from "../components/modalWindows/Board/EditBoard";
+export { default as Column } from "../components/Column/Column";
+export { default as Todo } from "../components/Todo/Todo";

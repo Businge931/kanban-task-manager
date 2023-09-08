7f2e79a451d210e4b536394ebbf3178ca36b2028
Businge931 <jimmybusinge9@gmail.com>
2023-09-08 22:33:35 +0300

    feat: add readonly prop on input field

--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ placeholder, onChange, value, defaultValue, id }) => {
+const Input = ({
+  placeholder,
+  onChange,
+  value,
+  defaultValue,
+  id,
+  readOnly,
+}) => {
   return (
     <input
       placeholder={placeholder}
@@ -10,6 +17,7 @@ const Input = ({ placeholder, onChange, value, defaultValue, id }) => {
       value={value}
       defaultValue={defaultValue}
       id={id}
+      readOnly={readOnly}
     />
   );
 };

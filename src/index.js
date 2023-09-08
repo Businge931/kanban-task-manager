2bb7418d4e0bafcd17a7a2a9a097da6c4c8423da
Businge931 <jimmybusinge9@gmail.com>
2023-09-08 22:30:31 +0300

    fix: edit context imports

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ModalContext from "./context/ModalContext";
+import { ModalProvider } from "./context/ModalContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { TasksProvider } from "./context/TasksContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <TasksProvider>
-        <ModalContext>
+    <TasksProvider>
+      <ThemeProvider>
+        <ModalProvider>
           <App />
-        </ModalContext>
-      </TasksProvider>
-    </ThemeProvider>
+        </ModalProvider>
+      </ThemeProvider>
+    </TasksProvider>
   </React.StrictMode>
 );

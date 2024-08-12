a00fbd9e3bc960ccfe4489234d11dd48ca648642
Businge931 <jimmybusinge9@gmail.com>
2024-08-12 15:26:41 +0300

    added new column

--- src/components/modalWindows/Board/EditBoard.jsx
+++ src/components/modalWindows/Board/EditBoard.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AddNewBoard.module.css";
 import Modal from "../modal/Modal";
 import Input from "../../ui/Input/Input";
 import cancel from "../../../assets/cancel.svg";
-import { UseModalContext } from "../../../context/ModalContext";
+import { useTasksContext } from "../../../context";
 
 const EditBoard = () => {
-  // const [name, setName] = useState("");
+  const [columnName, setColumnName] = useState("");
+  const [newColumns, setNewColumns] = useState([]);
 
-  const { columns, dispatch } = UseModalContext();
-  console.log(columns);
+  const { addNewColumn, boardName } = useTasksContext();
+
+  const newColumn = { name: columnName, tasks: [] };
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    addNewColumn(newColumn);
   }
 
-  function addNewColumn(e) {
-    e.preventDefault();
-    dispatch({
-      type: "addColumn",
-      payload: "",
-    });
+  function handleAddNewColumn() {
+    setNewColumns((prevCols) => [...prevCols, newColumn]);
+    setColumnName("");
+  }
+
+  function handleDeleteColumn(index) {
+    const updatedColumns = [...newColumns];
+    updatedColumns.splice(index, 1);
+    setNewColumns(updatedColumns);
   }
 
   return (
@@ -29,21 +36,39 @@ const EditBoard = () => {
         <h4>Edit Board</h4>
         <form onSubmit={handleSubmit}>
           <label>Name</label>
-          <Input placeholder="e.g. Web Design" defaultValue="" />
+          <Input defaultValue={boardName} readOnly={true} />
 
           <label>Columns</label>
-          {columns?.map((column, index) => (
+          {newColumns?.map((column, index) => (
             <div className={styles.columns} key={index}>
-              <Input placeholder="e.g. Drink coffee & smile" />
-              <img alt="cancel icon" src={cancel} />
+              <Input
+                placeholder="e.g. Drink coffee & smile"
+                value={column.name}
+                onChange={(e) => {
+                  const updatedColumns = [...newColumns];
+                  updatedColumns[index].name = e.target.value;
+                  setNewColumns(updatedColumns);
+                }}
+              />
+              <img
+                alt="cancel icon"
+                src={cancel}
+                onClick={() => handleDeleteColumn(index)}
+              />
             </div>
           ))}
 
           <div className={styles.buttons}>
-            <button id={styles.add_newColumn} onClick={addNewColumn}>
+            <button
+              type="button"
+              id={styles.add_newColumn}
+              onClick={handleAddNewColumn}
+            >
               + Add New Column
             </button>
-            <button id={styles.create_newBoard}>Save changes</button>
+            <button type="submit" id={styles.create_newBoard}>
+              Save changes
+            </button>
           </div>
         </form>
       </div>
